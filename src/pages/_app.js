d34f728fd3759710fb515fa0c0351d7a863f9e42
Tamaki-Dayo <126759548+Tamaki-Dayo@users.noreply.github.com>
2023-08-03 08:22:27 +1000

    Update 0.0.4

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/global.css'
+import { Comfortaa } from 'next/font/google'
+
+const comfortaa = Comfortaa({
+  subsets: ['latin'],
+  weight: ['700']
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={comfortaa.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
