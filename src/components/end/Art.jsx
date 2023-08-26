23b78e31aead62af6461806990212c7754465bd5
Claudio D. Morales <mclaudiod@gmail.com>
2023-08-25 21:06:00 -0300

    update 2.0.4

--- src/components/end/Art.jsx
+++ src/components/end/Art.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Art({handleArtNext}) {
+  const [loaded, setLoaded] = useState(false);
+
+  const handleLoaded = () => setLoaded(true);
+
   return (
     <Image
       src={`/shrine-exterior.png`}
       alt="Full Screen Image"
       fill 
       style={{objectFit: "contain"}}
-      onClick={handleArtNext}
+      onLoadingComplete={() => handleLoaded()}
+      onClick={loaded ? handleArtNext : ""}
     />
-  )
-}
+  );
+};
