7f7e58b738eecd81659049d3cf1c78fb35fe5954
Claudio D. Morales <mclaudiod@gmail.com>
2023-08-25 21:11:16 -0300

    update 2.0.5

--- src/components/end/Art2.jsx
+++ src/components/end/Art2.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
+import { useState } from 'react';
 
 export default function Art2({handleArtNext}) {
+  const [loaded, setLoaded] = useState(false);
+
+  const handleLoaded = () => setLoaded(true);
+
   return (
     <Image
-        src={`/birthday-party.png`}
-        alt="Full Screen Image"
-        fill 
-        style={{objectFit: "contain"}}
-        onClick={handleArtNext}
+      src={`/birthday-party.png`}
+      alt="Full Screen Image"
+      fill 
+      style={{objectFit: "contain"}}
+      onLoadingComplete={() => handleLoaded()}
+      onClick={loaded ? handleArtNext : ""}
     />
-  )
-}
+  );
+};
